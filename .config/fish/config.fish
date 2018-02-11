8a468897b5f6335156199481dd48a2c6f01218e1
Olya Piatrova <olkinn@ymail.com>
2018-02-11 16:29:30 +0300

    add tmuxline, tmuxconf, fishfile and upd config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,22 +1,3 @@
-# Path to your oh-my-fish.
-set fish_path $HOME/.oh-my-fish
-
-# Path to your custom folder (default path is ~/.oh-my-fish/custom)
-#set fish_custom $HOME/dotfiles/oh-my-fish
-
-# Load oh-my-fish configuration.
-. $fish_path/oh-my-fish.fish
-
-# Custom plugins and themes may be added to ~/.oh-my-fish/custom
-# Plugins and themes can be found at https://github.com/oh-my-fish/
-Theme 'cbjohnson'
-Plugin 'theme'
-Plugin 'grc'
-Plugin 'rbenv'
-Plugin 'z'
-# Private stuff
-source ~/.private/private.fish
-
 # Aliases
 alias :q=exit
 alias :wq=exit
